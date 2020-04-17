06b4af2cc5fb281bc26ae730a68d93a202ed78a2
Carlos Granell <carlos.granell.canut@gmail.com>
2020-04-17 15:28:34 +0200

    EM students - Questionnaire #1 done!

---
+++ install.R
@@ -0,0 +1,7 @@
+# Install CRAN packages
+install.packages("tidyverse")
+install.packages("forcats")
+install.packages("kableExtra")
+install.packages("here")
+install.packages("googlesheets4")
+install.packages("likert")
